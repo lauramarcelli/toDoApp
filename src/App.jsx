66cfd80e759e265b97ff9e56d8dbfbe33dbede9e
lorena252 <lorenaavalos252@hotmail.com>
2023-11-28 18:56:01 -0300

    agregue useState y cree una nueva rama

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import AddingTasks from "./AddingTasks";
 import List from "./List";
 import { Box, Container } from "@chakra-ui/react";
 import Flower from "./assets/rm428-0025.jpg";
+import { useState } from "react"
+
 
 function App() {
-  
+
+
+const [task, setTask] = useState([]) 
 
   return (
     <>
