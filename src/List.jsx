bf6c13302a17f460d136e2607c23792155fcfa97
lorena252 <lorenaavalos252@hotmail.com>
2023-11-28 20:43:11 -0300

    create the task container

--- src/List.jsx
+++ src/List.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { ButtonGroup, Container, Button, Text, Flex } from "@chakra-ui/react";
 
 export default function List() {
   return (
-    <></>
-  )
+    <Container>
+      <Flex
+        mt="2"
+        bg="pink"
+        color="black"
+        w="500px"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Text m="2" fontSize="xl">
+          Aca agregar el nombre de la tarea
+        </Text>
+        <ButtonGroup>
+          <Button colorScheme="teal" variant="solid" w="50px">
+            Hecho
+          </Button>
+          <Button colorScheme="teal" variant="solid" w="50px">
+            Delete
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </Container>
+  );
 }
