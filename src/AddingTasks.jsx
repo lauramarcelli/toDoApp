fcd2624e54525fc5d9feab6b7a7ac4fef59629f7
lorena252 <lorenaavalos252@hotmail.com>
2023-11-28 22:47:20 -0300

    merge rama  add-task

--- src/AddingTasks.jsx
+++ src/AddingTasks.jsx
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { Box, Flex, Input, Select, Button } from "@chakra-ui/react";
 
 export default function AddingTasks() {
-  return <></>;
+
+
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.title) {
+        errors.title = "Required";
+      } else if (values.title.length > 15 || values.title.length < 4) {
+        errors.title = "El titulo debe ser entre 4 y 15 caracteres";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <form
+      onSubmit={formik.handleSubmit}
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <Flex gap="20" justifyContent="center">
+        <Box bg="white" w="40%" h="100px" m="6" p="4">
+          <label htmlFor="title">Tarea</label>
+          <Input
+            placeholder="Ingresa una tarea"
+            id="title"
+            name="title"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+          {formik.errors.title ? <div>{formik.errors.title}</div> : null}
+        </Box>
+
+        <Box bg="white" w="40%" h="100px" m="6" p="4">
+          <h1>Seleccionar</h1>
+          <Select>
+            <option value="Todas">Todas</option>
+            <option value="Completas">Completas</option>
+            <option value="Incompletas">Incompletas</option>
+          </Select>
+        </Box>
+      </Flex>
+
+      <Button type="submit" colorScheme="teal" variant="solid" w="150px">
+        Send
+      </Button>
+    </form>
+  );
 }
