8157c1ae8c2e6df53bd1a648dcc21d86cdbc714e
lauramarcelli <laura.marcelli@gmail.com>
2023-11-29 11:29:55 -0300

    working on localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import AddingTasks from "./AddingTasks";
 import List from "./List";
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Flower from "./assets/rm428-0025.jpg";
 import { useState } from "react"
 
@@ -9,14 +9,14 @@ import { useState } from "react"
 function App() {
 
 
-const [task, setTask] = useState([]) 
+const [task, setTask] = useState(JSON.parse(localStorage.getItem('task'))||"")
 
   return (
     <>
       <Box bgImage={Flower} h="100vh" bgPosition="center" bgSize="cover" bgRepeat="no-repeat">
         <Header />
-        <AddingTasks />
-        <List />
+        <AddingTasks setTask={setTask} task={task}/>
+        <List task={task}/>
      </Box>
       </>
   );
